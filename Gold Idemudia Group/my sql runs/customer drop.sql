0803c0d562aad171c69791ebec11f0950bbf027b
Joshua <jidemudia@doorfunds.com>
2020-05-26 20:17:46 +0100

    added Gold idemudia group

---
+++ Gold Idemudia Group/my sql runs/customer drop.sql
@@ -0,0 +1,27 @@
+
+
+DROP TABLE sales.customers;
+
+ 
+
+ 
+
+CREATE TABLE customers                                                    
+
+(  
+
+    customer_id INT,  
+
+    first_name varchar(255),  
+
+    last_name varchar(255),  
+
+    email_address varchar(255),   
+
+    number_of_complaints int,   
+
+primary key (customer_id)   
+
+);  
+
+  
